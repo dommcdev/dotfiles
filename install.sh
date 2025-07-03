bbb4c7e9c0926615bcadb696455ec12ff94a7b82
Dominic <dominicmcdev@gmail.com>
2025-07-03 18:31:33 -0500

    edited todo and install

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/env bash
 
 # Prerequisites
-# sudo pacman -Syu git
+# sudo pacman -S git
 # mkdir dev && cd dev
 # git clone http://developermcd.com:8080/Dominic/dotfiles.git
 
@@ -17,19 +17,17 @@ echo "Installing yay..."
 sudo pacman -S --noconfirm --needed git base-devel
 git clone https://aur.archlinux.org/yay.git && cd yay
 makepkg -si --noconfirm && cd .. && rm -rf yay
-#yay --version
 
 # Install flatpak
 echo "Installing flatpak..."
 sudo pacman -S --noconfirm --needed flatpak
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-#flatpak --version
 
 # Install Hyprland core packages
-sudo pacman -S --noconfirm --needed hyprland dunst uwsm xdg-desktop-portal-hyprland qt5-wayland qt6-wayland hyprpolkitagent grim slurp sddm
+sudo pacman -S --noconfirm --needed dunst uwsm xdg-desktop-portal-hyprland qt5-wayland qt6-wayland hyprpolkitagent grim slurp sddm #hyprland
 
 # Install arch packages
-sudo pacman -S --noconfirm --needed ghostty nautilus nautilus-python fastfetch hyprpaper hypridle waybar rofi ttf-jetbrains-mono-nerd stow zsh btop virt-manager blueman neovim cliphist hyprpicker hyprsunset adw-gtk-theme
+sudo pacman -S --noconfirm --needed ghostty nautilus nautilus-python fastfetch hyprpaper hypridle waybar rofi ttf-jetbrains-mono-nerd stow zsh btop virt-manager blueman neovim cliphist hyprpicker hyprsunset adw-gtk-theme pulseaudio networkmanager
 
 # Install aur packages
 yay -S --noconfirm --needed --answerdiff None --answerclean None google-chrome
@@ -37,16 +35,6 @@ yay -S --noconfirm --needed --answerdiff None --answerclean None google-chrome
 # Install flatpaks
 flatpak install --noninteractive flathub org.gimp.GIMP org.onlyoffice.desktopeditors org.nickvision.tubeconverter org.gtk.Gtk3theme.adw-gtk3
 
-# -------------------------------------------------- #
-#                   SERVICE MANAGEMENT               #
-# -------------------------------------------------- #
-
-#Enable various services to start on boot
-sudo systemctl enable --now sddm.service
-#sudo systemctl enable --now bluetooth.service
-systemctl --user enable --now hyprpolkitagent.service
-systemctl --user enable --now hyprpaper.service
-systemctl --user enable --now waybar.service
 
 # -------------------------------------------------- #
 #                     CONFIGURATION                  #
@@ -55,7 +43,28 @@ systemctl --user enable --now waybar.service
 git config --global credential.helper store
 cd ~/dev/dotfiles
 git pull
-stow --adopt ghostty gtk hyprland hyprpaper waybar rofi apps
+stow --adopt ghostty gtk hyprland hyprpaper waybar rofi apps zsh
 git restore .
-stow --override ghostty gtk hyprland hyprpaper waybar rofi apps
+stow --override ghostty gtk hyprland hyprpaper waybar rofi apps zsh 
 
+
+# -------------------------------------------------- #
+#                   ONE-TIME COMMANDS                #
+# -------------------------------------------------- #
+
+# Enable various services to start on boot
+sudo systemctl enable --now sddm.service
+#sudo systemctl enable --now bluetooth.service
+systemctl --user enable --now hyprpolkitagent.service
+systemctl --user enable --now hyprpaper.service
+systemctl --user enable --now waybar.service
+
+# Flatpak overrides
+sudo flatpak override --system --filesystem=xdg-config/gtk-4.0
+sudo flatpak override --system --filesystem=xdg-config/gtk-3.0
+sudo flatpak override --system --filesystem=xdg-data/themes
+
+echo "-----------------------------------DONE!!!-------------------------"
+
+#Todo
+#Add prompt for rebooting
