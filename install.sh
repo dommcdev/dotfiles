d140dde52ef59815779bb1943ac4242e999dec25
Dominic <dominicmcdev@gmail.com>
2025-07-09 17:03:59 -0500

    tweaked todo and install

--- install.sh
+++ install.sh
@@ -60,6 +60,7 @@ systemctl --user enable --now hyprpolkitagent.service
 systemctl --user enable --now hyprpaper.service
 systemctl --user enable --now hypridle.service
 systemctl --user enable --now waybar.service
+systemctl --user enable --now xdg-desktop-portal-gtk.service
 
 # Getting GTK theme to apply everywhere
 mkdir -p ~/.local/share/themes/
