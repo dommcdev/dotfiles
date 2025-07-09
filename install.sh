f6ec891b5a55b5a5cfefd0ae9f8e2f522ac3bed3
Dominic <dominicmcdev@gmail.com>
2025-07-09 17:31:43 -0500

    minor tweak

--- install.sh
+++ install.sh
@@ -43,9 +43,9 @@ flatpak install --noninteractive flathub org.gimp.GIMP org.onlyoffice.desktopedi
 git config --global credential.helper store
 cd ~/dev/dotfiles
 git pull
-stow --adopt -D ghostty gtk hyprland waybar rofi apps zsh
+stow --adopt --dotfiles ghostty gtk hyprland waybar rofi apps zsh
 git restore .
-stow --override -D ghostty gtk hyprland waybar rofi apps zsh
+stow --override --dotfiles ghostty gtk hyprland waybar rofi apps zsh
 hyprctl reload
 
 
