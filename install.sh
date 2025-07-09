6f209cf5cde51e3e7daf713d38159f3ed3554af1
Dominic <dominicmcdev@gmail.com>
2025-07-09 16:31:28 -0500

    updated todo and install

--- install.sh
+++ install.sh
@@ -27,7 +27,7 @@ flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flat
 sudo pacman -S --noconfirm --needed dunst uwsm xdg-desktop-portal-hyprland xdg-desktop-portal-gtk qt5-wayland qt6-wayland hyprpolkitagent grim slurp sddm hyprland
 
 # Install arch packages
-sudo pacman -S --noconfirm --needed ghostty nautilus nautilus-python fastfetch hyprpaper hypridle waybar rofi ttf-jetbrains-mono-nerd stow zsh btop virt-manager blueman neovim cliphist hyprpicker hyprsunset adw-gtk-theme pavucontrol networkmanager
+sudo pacman -S --noconfirm --needed ghostty nautilus nautilus-python fastfetch hyprpaper hypridle waybar rofi ttf-jetbrains-mono-nerd stow zsh btop virt-manager blueman neovim cliphist hyprpicker hyprsunset adw-gtk-theme pavucontrol networkmanager network-manager-applet nm-connection-editor fzf ddcutil
 
 # Install aur packages
 yay -S --noconfirm --needed --answerdiff None --answerclean None google-chrome
@@ -43,9 +43,10 @@ flatpak install --noninteractive flathub org.gimp.GIMP org.onlyoffice.desktopedi
 git config --global credential.helper store
 cd ~/dev/dotfiles
 git pull
-stow --adopt ghostty gtk hyprland hyprpaper waybar rofi apps zsh
+stow --adopt ghostty gtk hyprland waybar rofi apps zsh
 git restore .
-stow --override ghostty gtk hyprland hyprpaper waybar rofi apps zsh 
+stow --override ghostty gtk hyprland waybar rofi apps zsh
+hyprctl reload
 
 
 # -------------------------------------------------- #
@@ -57,11 +58,13 @@ sudo systemctl enable --now sddm.service
 #sudo systemctl enable --now bluetooth.service
 systemctl --user enable --now hyprpolkitagent.service
 systemctl --user enable --now hyprpaper.service
+systemctl --user enable --now hypridle.service
 systemctl --user enable --now waybar.service
 
 # Getting GTK theme to apply everywhere
 mkdir -p ~/.local/share/themes/
 ln -s /usr/share/themes/adw-gtk3 ~/.local/share/themes/adw-gtk3
+rm -rf ~/.config/gtk-4.0 ~/.config/gtk-3.0
 flatpak override --user --filesystem=xdg-config/gtk-4.0
 flatpak override --user --filesystem=xdg-config/gtk-3.0
 flatpak override --user --filesystem=xdg-data/themes
@@ -79,7 +82,7 @@ read -rp "Do you want to reboot? [Y/n]: " choice
 choice=${choice,,}
 
 if [[ "$choice" == "y" || "$choice" == "" ]]; then
-    echo "Rebooting..."
+    echo "Rebooting now..."
     sudo reboot now
 else
     echo "Reboot as soon as possible for all configurations to take effect."
