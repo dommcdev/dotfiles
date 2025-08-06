ab0a6bff9d5001d2f8c82c1a0bcfb4bcaa614dcf
Dominic <dominicmcdev@gmail.com>
2025-08-06 14:12:09 -0500

    moving stuff from shell.sh over to dotbot configs

--- meta/shell.sh
+++ meta/shell.sh
@@ -1,28 +1,13 @@
 #!/bin/env bash
 
 
-# # Make sure system is up to date
-sudo pacman -Syu
+# Terminal
+sudo pacman -S --noconfirm --needed openssh wget curl ffmpeg 7zip fd ripgrep poppler zoxide imagemagick chafa resvg ncdu fzf trash-cli bat cava asciiquarium cmatrix cowsay ponysay tldr whois
 
-# Install yay
-echo "Installing yay..."
-sudo pacman -S --noconfirm --needed git base-devel
-git clone https://aur.archlinux.org/yay.git && cd yay
-makepkg -si --noconfirm && cd .. && rm -rf yay
-
-# Install flatpak
-echo "Installing flatpak..."
-sudo pacman -S --noconfirm --needed flatpak
-flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-
-# Install graphics drivers
-sudo pacman -S --noconfirm --needed libva-mesa-driver libva-utils mesa vulkan-radeon
-
-# Install useful utilies
-sudo pacman -S --noconfirm --needed openssh wget iwd wireless_tools wpa_supplicant smartmontools xdg-utils bluez bluez-utils curl ffmpeg gst-plugins-good gst-plugins-bad gst-plugins-ugly gst-libav jq 7zip fd ripgrep poppler zoxide imagemagick chafa resvg qt6-svg qt6-declarative qt5-quickcontrols2 udiskie inetutils iwd ncdu
+# Shared
 
 # Install arch packages
-sudo pacman -S --noconfirm --needed dunst uwsm xdg-desktop-portal-hyprland xdg-desktop-portal-gtk qt5-wayland qt6-wayland hyprpolkitagent grim slurp sddm hyprland hyprlock hypridle hyprpaper hyprpicker hyprsunset ghostty nautilus nautilus-python gvfs-smb fastfetch waybar rofi ttf-jetbrains-mono-nerd stow zsh btop virt-manager blueman neovim adw-gtk-theme pavucontrol networkmanager network-manager-applet nm-connection-editor ddcutil tailscale yazi fzf libnotify trash-cli noto-fonts-emoji sushi bat cava asciiquarium cmatrix cowsay ponysay papirus-icon-theme tldr whois 
+sudo pacman -S --noconfirm --needed dunst  grim slurp sddm ghostty nautilus nautilus-python gvfs-smb fastfetch waybar rofi ttf-jetbrains-mono-nerd zsh btop virt-manager blueman neovim adw-gtk-theme pavucontrol networkmanager network-manager-applet nm-connection-editor ddcutil tailscale yazi libnotify noto-fonts-emoji sushi papirus-icon-theme  udiskie
 
 # Install gui packages
 sudo pacman -S --noconfirm --needed gimp impression audacity gnome-calculator decibels papers loupe showtime switcheroo gnome-calendar libreoffice-fresh blender, kdenlive
@@ -39,12 +24,6 @@ source "$HOME/.config/nvm/nvm.sh" #source file in lieu of restarting the shell
 nvm install 22
 npm install -g @google/gemini-cli
 
-# -------------------------------------------------- #
-#                     CONFIGURATION                  #
-# -------------------------------------------------- #
-
-# Ensure some dirs exist (or don't exist)
-mkdir -p ~/Downloads
 
 
 
@@ -52,9 +31,6 @@ mkdir -p ~/Downloads
 sudo systemctl enable sddm.service
 sudo systemctl enable --now bluetooth.service
 sudo systemctl enable --now tailscaled
-systemctl --user enable hyprpolkitagent.service
-systemctl --user enable hyprpaper.service
-systemctl --user enable hypridle.service
 systemctl --user enable waybar.service
 systemctl --user enable sunshine.service
 
@@ -74,17 +50,11 @@ git clone https://github.com/vinceliuice/Colloid-icon-theme.git && cd Colloid-ic
 cd .. && rm -rf Colloid-icon-them
 gsettings set org.gnome.desktop.interface icon-theme "Colloid-Yellow-Catppuccin-Dark"
 
-# Apply grub theme/config
-#sudo mv /etc/default/grub /etc/default/grub.bak
-#cd ~/dev/dotfiles && sudo stow --dotfiles --target=/ grub
-#sudo grub-mkconfig -o /boot/grub/grub.cfg
-
 
 # Authenticate Tailscale (also sign in to Google in the process)
 #sudo tailscale up
 
 
-echo "-----------------------------------DONE!!!-------------------------"
 
 read -rp "Do you want to reboot? [Y/n]: " choice
 
