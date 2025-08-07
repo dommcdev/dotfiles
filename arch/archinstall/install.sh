9421983532c88859d1d1ad5ac82fa2ad160cfda6
Dominic <dominicmcdev@gmail.com>
2025-08-07 11:07:02 -0500

    edited archinstall bootstrap script, moved build-archiso to scripts folder

--- arch/archinstall/install.sh
+++ arch/archinstall/install.sh
@@ -1,26 +1,18 @@
 #!/bin/env bash
 set -e
 
-echo "This script will install Arch Linux on the specified drive (via archinstall), as well as clone a dotfiles repo to ~/dev/dotfiles."
+echo "This script will install Arch Linux on the specified drive (chosen via archinstall), as well as clone a dotfiles repo to ~/dev/dotfiles."
 
 # Read in git username and password securely
 read -p "Please enter your git username: " GIT_USER
 read -s -p "Please enter your git password: " GIT_PASS
+echo -e "\n\n"
 
 # Run archinstall with included configuration/credential file. Disk input is still manual.
 archinstall --config /root/user_configuration.json --creds /root/user_credentials.json
 echo -e "\n\n--- Archinstall completed successfully. Cloning dotfiles via arch-chroot ... ---\n\n"
 
 # Clone dotfiles repo and install dotfiles
-cat << 'EOF' > /mnt/home/dominic/bootstrap-dotfiles.sh
-#!/bin/env bash
-mkdir -p ~/dev/
-git clone http://$GIT_USER:$GIT_PASS@developermcd.com:8080/dominic/dotfiles.git ~/dev/dotfiles
-cd dotfiles && ./install_profile arch-main
-EOF
-arch-chroot /mnt sudo -u dominic bash /home/dominic/bootstrap_dotfiles.sh
-arch-chroot /mnt rm /home/dominic/bootstrap_dotfiles.sh
-
-
-#arch-chroot /mnt su -l dominic -c 'mkdir -p ~/dev/'
-#arch-chroot /mnt su -l dominic -c 'git clone http://$GIT_USER:$GIT_PASS@developermcd.com:8080/dominic/dotfiles.git ~/dev/dotfiles'
+echo "git clone http://$GIT_USER:$GIT_PASS@developermcd.com:8080/dominic/dotfiles.git ~/dev/dotfiles" > /mnt/home/dominic/tmp.sh
+arch-chroot /mnt sudo -u dominic bash home/dominic/tmp.sh
+arch-chroot /mnt rm home/dominic/tmp.sh
