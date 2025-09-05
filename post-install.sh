091f8f7d7d5401b7ba62ca103817166173465a6a
Dominic <dominicmcdev@gmail.com>
2025-09-05 17:58:00 -0500

    added semi-fixed timeshift-gtk.desktop

--- post-install.sh
+++ post-install.sh
@@ -3,4 +3,4 @@
 # Authenticate Tailscale (also sign in to Google in the process)
 sudo tailscale up
 
-
+# Set up nextcloud-client downloads folder sync. Ignore the first setup wizard - it won't work.
