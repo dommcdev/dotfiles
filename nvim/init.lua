95f3c2bac2f6a078b9906d078b4f6b99e423adaf
Dominic <dominicmcdev@gmail.com>
2025-09-11 16:11:38 -0500

    tweaked nvim config, updated todo

--- nvim/init.lua
+++ nvim/init.lua
@@ -4,6 +4,8 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
+vim.opt.signcolumn="yes"
+
 vim.g.have_nerd_font = true
 vim.o.number = true
 vim.o.relativenumber = true
@@ -75,6 +77,22 @@ end
 ---@type vim.Option
 local rtp = vim.opt.rtp
 rtp:prepend(lazypath)
+
+
+-- Example: apply your Neovim settings to all LSP formatters
+vim.api.nvim_create_autocmd("LspAttach", {
+  callback = function(args)
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+    if client and client.server_capabilities.documentFormattingProvider then
+      client.config.settings = client.config.settings or {}
+      client.config.settings.format = client.config.settings.format or {}
+      client.config.settings.format = vim.tbl_deep_extend("force", client.config.settings.format, {
+        tabSize = vim.bo[args.buf].shiftwidth,
+        insertSpaces = vim.bo[args.buf].expandtab,
+      })
+    end
+  end,
+})
 
 
 -- Load and setup your plugins
