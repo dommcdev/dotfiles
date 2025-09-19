81cfc12fb9ef50c78ed22449fad6a253b8a22c72
Dominic <dominicmcdev@gmail.com>
2025-09-18 19:38:28 -0500

    add tmux-sessionizer to workflow, convert zsh configs so macos is override rather than copy of conf, add fzf config

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -1,3 +1,9 @@
 vim.api.nvim_create_user_command("ClangFormat", function()
     vim.cmd('!clang-format -i -style="{BasedOnStyle: Google, IndentWidth: 4}" %')
 end, {})
+
+vim.keymap.set("n", "<C-t>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
+vim.keymap.set("n", "<M-h>", "<cmd>silent !tmux neww tmux-sessionizer -s 0<CR>")
+vim.keymap.set("n", "<M-j>", "<cmd>silent !tmux neww tmux-sessionizer -s 1<CR>")
+vim.keymap.set("n", "<M-k>", "<cmd>silent !tmux neww tmux-sessionizer -s 2<CR>")
+vim.keymap.set("n", "<M-l>", "<cmd>silent !tmux neww tmux-sessionizer -s 3<CR>")
