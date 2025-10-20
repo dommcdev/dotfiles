f4a7f0c706c57f15f7bccf1af43d146da2bc916c
Dominic <dominicmcdev@gmail.com>
2025-10-20 15:57:19 -0500

    fix tailscale installation
    tailscale set doesn't work until tailscale is running

--- post-install.sh
+++ post-install.sh
@@ -1,6 +1,6 @@
 #!/bin/env bash
 
 # Authenticate Tailscale (also sign in to Google in the process)
-sudo tailscale up
+sudo tailscale up --accept-routes --reset
 
 # Set up nextcloud-client downloads folder sync. Ignore the first setup wizard - it won't work.
