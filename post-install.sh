9249273f9c959a676e6569dd1d701bf2d664b476
Dominic <dominicmcdev@gmail.com>
2025-10-20 20:38:46 -0500

    add most of davfs2 conf, add comment to post-install, add alias for full fastfetch

--- post-install.sh
+++ post-install.sh
@@ -4,3 +4,4 @@
 sudo tailscale up --accept-routes --reset
 
 # Set up nextcloud-client downloads folder sync. Ignore the first setup wizard - it won't work.
+# Set up sunshine - open with rofi, then go to localhost:47990 and set up creds
