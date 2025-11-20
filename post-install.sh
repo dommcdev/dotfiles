d4670646b326fea3877bc380d31abe4aed765207
Dominic <dominicmcdev@gmail.com>
2025-11-19 22:57:26 -0600

    added ssh and operator flags into tailscale setup cmds

--- post-install.sh
+++ post-install.sh
@@ -1,7 +1,8 @@
 #!/bin/env bash
 
 # Authenticate Tailscale (also sign in to Google in the process)
-sudo tailscale up --accept-routes --reset
+sudo tailscale set --operator=$USER
+tailscale up --accept-routes --reset --ssh
 
 # Set up nextcloud-client downloads folder sync. Ignore the first setup wizard - it won't work.
 # Set up sunshine - open with rofi, then go to localhost:47990 and set up creds
