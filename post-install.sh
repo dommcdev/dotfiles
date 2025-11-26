6960388c38ac7c6ae8d4497629ae63112bd3f93c
Dominic <dominicmcdev@gmail.com>
2025-11-25 23:55:59 -0600

    fix bugs

--- post-install.sh
+++ post-install.sh
@@ -1,8 +1,9 @@
 #!/bin/env bash
 
 # Authenticate Tailscale (also sign in to Google in the process)
+sudo tailscale up --accept-routes --reset --ssh
 sudo tailscale set --operator=$USER
-tailscale up --accept-routes --reset --ssh
 
 # Set up nextcloud-client downloads folder sync. Ignore the first setup wizard - it won't work.
 # Set up sunshine - open with rofi, then go to localhost:47990 and set up creds
+# Set up timeshift - open, pick disk to snapshot
