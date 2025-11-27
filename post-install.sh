10bad717261039b99edbe6a3c9c2113e1a7cee14
Dominic <dominicmcdev@gmail.com>
2025-11-26 18:34:46 -0600

    add laptop stuff

--- post-install.sh
+++ post-install.sh
@@ -4,6 +4,7 @@
 sudo tailscale up --accept-routes --reset --ssh
 sudo tailscale set --operator=$USER
 
+# Set up chrome stuff - don't show cards/shortcuts, set wallpaper
 # Set up nextcloud-client downloads folder sync. Ignore the first setup wizard - it won't work.
-# Set up sunshine - open with rofi, then go to localhost:47990 and set up creds
+# Set up sunshine - open with rofi, then go to localhost:47990 and set up creds (same as system)
 # Set up timeshift - open, pick disk to snapshot
