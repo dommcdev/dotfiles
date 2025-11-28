527bde03061a14cf111589bf92fbc7f6325517e8
Dominic <dominicmcdev@gmail.com>
2025-11-28 10:58:12 -0600

    broke tailscale off into its own yaml

--- post-install.sh
+++ post-install.sh
@@ -1,9 +1,7 @@
 #!/bin/env bash
 
 # Authenticate Tailscale (also sign in to Google in the process)
-# Tailscale note: At least when I'm on the same network as it, having a device configured as a subnet router off will result in tailscale not working. run tailscale up without the --accept-routes flag and it will work (have internet connectivity) again. 
-sudo tailscale up --accept-routes --reset --ssh
-sudo tailscale set --operator=$USER
+cd ~/dev/dotfiles && ./install-alone tailscale
 
 # Set up chrome stuff - don't show cards/shortcuts, set wallpaper
 # Set up nextcloud-client downloads folder sync. Ignore the first setup wizard - it won't work.
