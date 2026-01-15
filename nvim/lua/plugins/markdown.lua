dc84d2b9e8d522a07dc95274ebba8cd8c2bcbd9d
Dominic <dominicmcdev@gmail.com>
2026-01-14 21:21:34 -0600

    add markdown preview in browser, both via peek.nvim and a backup standalone gh markdown-preview

---
+++ nvim/lua/plugins/markdown.lua
@@ -0,0 +1,37 @@
+return {
+  "toppair/peek.nvim",
+  event = { "VeryLazy" },
+  build = "deno task --quiet build:fast",
+  keys = {
+    {
+      "<leader>mp",
+      function()
+        local peek = require("peek")
+        if peek.is_open() then
+          peek.close()
+        else
+          peek.open()
+        end
+      end,
+      desc = "Toggle Peek Preview",
+    },
+  },
+  opts = {
+    theme = "dark",
+    app = "browser",
+  },
+}
+
+-- Backup: markdown-preview.nvim
+-- return {
+--     "iamcco/markdown-preview.nvim",
+--     cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+--     ft = { "markdown" },
+--     build = "cd app && npm install",
+--     keys = {
+--         { "<leader>mp", "<cmd>MarkdownPreviewToggle<CR>", desc = "Toggle Markdown Preview" },
+--     },
+--     init = function()
+--         vim.g.mkdp_theme = "dark" -- or "light"
+--     end,
+-- }
