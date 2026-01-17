7b9863dbbe8215bfd45955af677b0693701037f6
Dominic <dominicmcdev@gmail.com>
2026-01-16 21:43:45 -0600

    update post-install to reflect new yaml config folder structure

--- arch/post-install.sh
+++ arch/post-install.sh
@@ -1,9 +1,8 @@
 #!/bin/env bash
 
 # Authenticate Tailscale (also sign in to Google in the process)
-cd ~/dev/dotfiles && ./install-alone tailscale
+cd ~/dev/dotfiles && ./setup all/tailscale
 
 # Set up chrome stuff - don't show cards/shortcuts, set wallpaper
 # Set up nextcloud-client downloads folder sync. Ignore the first setup wizard - it won't work.
-# Set up sunshine - open with rofi, then go to localhost:47990 and set up creds (same as system)
 # Set up timeshift - open, pick disk to snapshot
