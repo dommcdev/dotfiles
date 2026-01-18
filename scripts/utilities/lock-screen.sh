b737aac75f76ca5d62d2ba2efbedd46644923c29
Dominic <dominicmcdev@gmail.com>
2026-01-17 18:22:39 -0600

    lots of misc stuff

--- scripts/utilities/lock-screen.sh
+++ scripts/utilities/lock-screen.sh
@@ -3,7 +3,7 @@
 # Locks the system using hyprlock, but not before ensuring the screensaver stopped.
 
 # Lock the screen
-pidof hyprlock || hyprlock --immediate --no-fade-in &
+pidof hyprlock || hyprlock &
 
 # Avoid running screensaver when locked
-pkill -f com.dominic.screensaver
+sleep 2; pkill -f com.dominic.screensaver
