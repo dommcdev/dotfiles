c0bb077f384f4e10aa6c1d49e5126d45d045e263
Dominic <dominicmcdev@gmail.com>
2026-01-17 21:59:47 -0600

    fix hypridle/hyprlock issues somewhat, take notes this time

--- scripts/utilities/lock-screen.sh
+++ scripts/utilities/lock-screen.sh
@@ -7,5 +7,10 @@ pidof hyprlock && exit 0
 pkill -f com.dominic.screensaver 2>/dev/null || true
 sleep 0.1
 
-hyprlock --immediate-render --no-fade-in
+hyprlock --no-fade-in &
 
+#Notes
+# idle_inhibit=3 in hypridle.conf makes the system wait for hyprlock to be "finished" before sleeping
+# however, hyprlock reports being done a little prematurely - without the --no-fade-in flag the system will sleep
+# before the lockscreen has fully rendered, resulting in a ghost half-lockscreen half-desktop image upon waking
+# --immediate-render flag will render something immeditely, even if it is just a background color and white input box (no clock, etc)
