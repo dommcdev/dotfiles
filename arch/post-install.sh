6e2f4976d0598f097629c0dd502b6f4f7b756a58
Dominic <dominicmcdev@gmail.com>
2026-01-21 16:17:38 -0600

    tweaking install stuff

--- arch/post-install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/env bash
-
-# Authenticate Tailscale (also sign in to Google in the process)
-cd ~/dev/dotfiles && ./setup all/tailscale
-bash ~/.local/bin/enroll-fingerprints
-
-# Set up chrome stuff - don't show cards/shortcuts, set wallpaper
-# Set up nextcloud-client downloads folder sync. Ignore the first setup wizard - it won't work.
-# Set up timeshift - open, pick disk to snapshot
---
+++ scripts/utilities/post-install.sh
@@ -0,0 +1,8 @@
+#!/bin/env bash
+
+xdg-open https://accounts.google.com
+tailscale up
+bash ~/.local/bin/enroll-fingerprints
+
+# Set up chrome stuff - don't show cards/shortcuts, set wallpaper
+# Set up timeshift - open, pick disk to snapshot
