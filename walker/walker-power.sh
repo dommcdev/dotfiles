f170fb230afc2de172903119b34e833d339ca44d
Dominic <dominicmcdev@gmail.com>
2026-01-22 00:52:48 -0600

    continue working on walker-power.sh

--- walker/walker-power.sh
+++ walker/walker-power.sh
@@ -1,26 +1,26 @@
 #!/usr/bin/env bash
 
-# Power menu using Walker
-# Adapted from omarchy-menu
-
 menu() {
   local prompt="$1"
   local options="$2"
 
   # walker dmenu mode
-  echo -e "$options" | walker --dmenu --width 295 --minheight 1 --maxheight 630 -p "$prompt"
+  echo -e "$options" | walker --dmenu --width 295 --minheight 1 --maxheight 730 -p "$prompt"
 }
 
 show_system_menu() {
-  local options="󰐥  Shutdown\n󰜉  Restart\n󰒲  Suspend\n  Lock\n󱄄  Screensaver\n󱄄  Lava Lamp\n"
+  # Removed trailing newline to prevent empty extra item
+  local options="󰐥  Shutdown\n󰜉  Reboot\n󰒲  Suspend\n󰍃  Log Out\n  Lock\n󱄄  Screensaver\n󱄄  Lava Lamp\n󰜉  Reboot to UEFI\n"
 
   case $(menu "System" "$options") in
+  *Shutdown*) systemctl poweroff ;;
+  *Reboot*) systemctl reboot ;;
+  *Suspend*) systemctl suspend ;;
+  *Logout*) uwsm stop ;;
   *Lock*) loginctl lock-session ;;
   *Screensaver*) launch-screensaver.sh ;;
   *Lavalamp*) launch-screensaver.sh -l ;;
-  *Suspend*) systemctl suspend ;;
-  *Restart*) systemctl reboot ;;
-  *Shutdown*) systemctl poweroff ;;
+  *Rebootuefi*) systemctl reboot --firmware-setup;;
   esac
 }
 
