7994e8a716421f1a33f94076fab9e68a8d694ce2
Dominic <dominicmcdev@gmail.com>
2026-01-22 01:00:17 -0600

    more changes to walker-power

--- walker/walker-power.sh
+++ walker/walker-power.sh
@@ -5,12 +5,12 @@ menu() {
   local options="$2"
 
   # walker dmenu mode
-  echo -e "$options" | walker --dmenu --width 295 --minheight 1 --maxheight 730 -p "$prompt"
+  echo -e "$options" | walker --dmenu --width 295 -p "$prompt"
 }
 
 show_system_menu() {
   # Removed trailing newline to prevent empty extra item
-  local options="󰐥  Shutdown\n󰜉  Reboot\n󰒲  Suspend\n󰍃  Log Out\n  Lock\n󱄄  Screensaver\n󱄄  Lava Lamp\n󰜉  Reboot to UEFI\n"
+  local options="󰐥  Shutdown\n󰜉  Reboot\n󰒲  Suspend\n󰍃  Log Out\n  Lock\n󱄄  Screensaver\n󱄄  Lava Lamp\n󰜉  Reboot to UEFI"
 
   case $(menu "System" "$options") in
   *Shutdown*) systemctl poweroff ;;
@@ -19,8 +19,8 @@ show_system_menu() {
   *Logout*) uwsm stop ;;
   *Lock*) loginctl lock-session ;;
   *Screensaver*) launch-screensaver.sh ;;
-  *Lavalamp*) launch-screensaver.sh -l ;;
-  *Rebootuefi*) systemctl reboot --firmware-setup;;
+  *Lava*Lamp*) launch-screensaver.sh -l ;;
+  *Reboot*UEFI*) systemctl reboot --firmware-setup ;;
   esac
 }
 
