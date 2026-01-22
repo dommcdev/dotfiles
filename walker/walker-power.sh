979e7fee1c5acea4c72c7b7661271ee56a6d90a9
Dominic <dominicmcdev@gmail.com>
2026-01-22 01:37:10 -0600

    getting walker power working nicely

--- walker/walker-power.sh
+++ walker/walker-power.sh
@@ -4,21 +4,24 @@ menu() {
   local prompt="$1"
   local options="$2"
 
-  echo -e "$options" | walker --dmenu --width 265 --maxwidth 265 -p "$prompt"
+  echo -e "$options" | walker --dmenu --width 265 --maxwidth 265 --minwidth 265 -p "$prompt"
 }
 
 show_system_menu() {
-  local options="󰐥  Shutdown                    poweroff\n󰜉  Reboot\n󰒲  Suspend\n󰍃  Log Out\n  Lock\n󱄄  Screensaver\n󱄄  Lava Lamp\n󰜉  Reboot to UEFI"
+  # Spacer to push aliases off-screen (requires item_dmenu.xml with ellipsize=0)
+  local s="                                                                                                    "
+
+  local options="󰐥  Shutdown${s}power off\n󰜉  Reboot${s}restart\n󰒲  Suspend${s}sleep\n󰍃  Log Out${s}logout\n  Lock\n󱄄  Screensaver\n󱄄  Lava Lamp\n󰜉  Reboot to UEFI${s}bios"
 
   case $(menu "System" "$options") in
   *Shutdown*) systemctl poweroff ;;
+  *Reboot*UEFI*) systemctl reboot --firmware-setup ;;
   *Reboot*) systemctl reboot ;;
   *Suspend*) systemctl suspend ;;
   *Logout*) uwsm stop ;;
   *Lock*) loginctl lock-session ;;
   *Screensaver*) launch-screensaver.sh ;;
   *Lava*Lamp*) launch-screensaver.sh -l ;;
-  *Reboot*UEFI*) systemctl reboot --firmware-setup ;;
   esac
 }
 
