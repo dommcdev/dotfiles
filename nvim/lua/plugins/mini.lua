870453a08dc508a773556612d8526d33103805ea
Dominic <dominicmcdev@gmail.com>
2026-01-24 18:54:46 -0600

    switch from mini statusline to lualine

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -3,9 +3,6 @@ return {
 		"nvim-mini/mini.nvim",
 		enabled = true,
 		config = function()
-			-- Statusline
-			require("mini.statusline").setup({ use_icons = true })
-
 			-- Autopairs
 			require("mini.pairs").setup()
 		end,
