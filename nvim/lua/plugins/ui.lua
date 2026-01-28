73d9e5640dbedba8a2fcb5d6fd53c63dad4c520e
Dominic <dominicmcdev@gmail.com>
2026-01-28 13:01:55 -0600

    way better command prompt in nvim

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -21,4 +21,21 @@ return {
       }
     end,
   },
+
+  -- Configure Noice to put the command line in the bottom left (classic cmdline)
+  {
+    "folke/noice.nvim",
+    opts = {
+      cmdline = {
+        enabled = true, -- Enable Noice cmdline
+        view = "cmdline", -- Use the classic bottom cmdline view
+      },
+      messages = {
+        enabled = true, -- Keep message handling enabled
+      },
+      popupmenu = {
+        enabled = true, -- Keep fancy popupmenu
+      },
+    },
+  },
 }
