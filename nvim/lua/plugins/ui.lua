d9bef6014aec03de07f1cefe073f77809c338e5b
Dominic <dominicmcdev@gmail.com>
2026-01-28 14:41:13 -0600

    keep whichkey enabled so it doesn't break stuff but make sure it doesn't pop up

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -2,8 +2,37 @@ return {
   -- Disable bufferline (tab bar)
   { "akinsho/bufferline.nvim", enabled = false },
 
-  -- Disable WhichKey
-  { "folke/which-key.nvim", enabled = false },
+  -- Disable WhichKey visual popup but keep plugin loaded
+  {
+    "folke/which-key.nvim",
+    enabled = true, -- Must be true for deps
+    event = "VeryLazy",
+    opts = {
+      -- Completely disable the automatic triggering
+      triggers = {
+        -- This empty table is the correct way to disable auto-triggers according to docs
+      },
+      plugins = {
+        marks = false,     -- shows a list of your marks on ' and `
+        registers = false, -- shows your registers on " in NORMAL or <C-r> in INSERT mode
+        spelling = {
+          enabled = false, -- enabling this will show WhichKey when pressing z= to select spelling suggestions
+        },
+        presets = {
+          operators = false,    -- adds help for operators like d, y, ...
+          motions = false,      -- adds help for motions
+          text_objects = false, -- help for text objects triggered after entering an operator
+          windows = false,      -- default bindings on <c-w>
+          nav = false,          -- misc bindings to work with windows
+          z = false,            -- bindings for folds, spelling and others prefixed with z
+          g = false,            -- bindings for prefixed with g
+        },
+      },
+    },
+    keys = {
+      -- Override default which-key toggle if you want
+    },
+  },
 
   -- Configure lualine to show modified status [+]
   {
