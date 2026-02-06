d93e03010c2e43f726f5d125d0cc57a2320309ce
Dominic <dominicmcdev@gmail.com>
2026-02-06 10:50:44 -0600

    move github auth to postinstall now that dotfiles are public

--- scripts/utilities/post-install.sh
+++ scripts/utilities/post-install.sh
@@ -2,6 +2,7 @@
 
 xdg-open https://accounts.google.com
 tailscale up
+bash ~/dev/dotfiles/setup dev/gitrepos
 bash ~/.local/bin/enroll-fingerprints
 
 # Set up chrome stuff - don't show cards/shortcuts, set wallpaper
