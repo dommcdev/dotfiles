09c04a42d1009d9d4f83d2aa7fe8744ce81e4cf8
Dominic <dominicmcdev@gmail.com>
2026-02-11 19:57:10 -0600

    add some nvim keymaps, update opencode.json to use latest tag for gemini antigravity auth

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -5,17 +5,29 @@ vim.keymap.set("n", "<M-k>", "<cmd>silent !tmux neww tmux-sessionizer -s 2<CR>")
 vim.keymap.set("n", "<M-l>", "<cmd>silent !tmux neww tmux-sessionizer -s 3<CR>")
 
 vim.keymap.set("n", "<leader>t", function()
-	Snacks.terminal.toggle(nil, {
-		cwd = vim.fn.expand("%:p:h"),
-		win = {
-			position = "float",
-			height = 0.9,
-			width = 0.9,
-			border = "rounded",
-		},
-	})
+  Snacks.terminal.toggle(nil, {
+    cwd = vim.fn.expand("%:p:h"),
+    win = {
+      position = "float",
+      height = 0.9,
+      width = 0.9,
+      border = "rounded",
+    },
+  })
 end, { desc = "Toggle Terminal" })
 
 -- Fast buffer switching with Shift+h and Shift+l
 vim.keymap.set("n", "L", ":bnext<CR>", { noremap = true, silent = true })
 vim.keymap.set("n", "H", ":bprevious<CR>", { noremap = true, silent = true })
+
+-- ThePrimeagen's remaps
+-- Keeps cursor centered when searching
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- Keeps cursor in place when joining lines
+vim.keymap.set("n", "J", "mzJ`z")
+
+-- Yank to system clipboard
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", 'gg"+yG')
