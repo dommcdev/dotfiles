dfbc43251f3579074885cf1de9421725b39c7ee7
Dominic <dominicmcdev@gmail.com>
2026-03-16 20:11:43 -0500

    add uv, ditch chpwd autovenv hook, disable typst nvim since I'm using latex for better ecosystem support

--- nvim/lua/plugins/typst.lua
+++ nvim/lua/plugins/typst.lua
@@ -2,8 +2,11 @@ return {
   {
     "chomosuke/typst-preview.nvim",
     ft = "typst",
+    enabled = false,
     version = "1.*",
-    build = function() require("typst-preview").update() end,
+    build = function()
+      require("typst-preview").update()
+    end,
     keys = {
       { "<leader>tp", "<cmd>TypstPreviewToggle<CR>", desc = "[T]ypst [P]review Toggle" },
     },
