3b18dab3025368a2e6071cc29d5b8da22632796c
Dominic <dominicmcdev@gmail.com>
2026-07-23 18:13:12 -0500

    use official adw-gtk3 flatpak theme, nuke pacman theme cp hook, fix docker libvirt fight

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -1,10 +1,10 @@
 return {
-	{
-		"nvim-mini/mini.nvim",
-		enabled = true,
-		config = function()
-			-- Autopairs
-			require("mini.pairs").setup()
-		end,
-	},
+  {
+    "nvim-mini/mini.nvim",
+    enabled = false,
+    config = function()
+      -- Autopairs
+      require("mini.pairs").setup()
+    end,
+  },
 }
