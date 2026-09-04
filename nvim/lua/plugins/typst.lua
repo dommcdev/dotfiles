66034dc60685380104c670da7bd5a7ba9eaed56f
Dominic McDevitt <dominicmcdev@gmail.com>
2026-09-04 17:29:43 -0500

    nvmind that didn't work

--- nvim/lua/plugins/typst.lua
+++ nvim/lua/plugins/typst.lua
@@ -1,17 +1,15 @@
 return {
   {
     "chomosuke/typst-preview.nvim",
-    -- ft-only: defer setup/download to first typst file; no `keys` so it can't early-load elsewhere.
     ft = "typst",
     version = "1.*",
     opts = {},
-    config = function(_, opts)
-      require("typst-preview").setup(opts)
-
-      vim.keymap.set("n", "<leader>tp", "<cmd>TypstPreviewToggle<CR>", {
+    keys = {
+      {
+        "<leader>tp",
+        "<cmd>TypstPreviewToggle<CR>",
         desc = "[T]ypst [P]review Toggle",
-        silent = true,
-      })
-    end,
+      },
+    },
   },
 }
